d068ce379209b9aa8279eae38bcf8b26527f462c
Joshua Alletto <joshalletto@gmail.com>
2018-10-23 13:38:45 -0500

    ruby to python

---
+++ python/bowling_spec.py
@@ -0,0 +1,10 @@
+import unittest
+from bowling import Game
+
+
+class TestGame(unittest.TestCase):
+
+  def setUp(self):
+    self.game = Game()
+
+# Your tests here. 
--- ruby/bowling_spec.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'bowling'
-
-describe "" do
-  
-end
