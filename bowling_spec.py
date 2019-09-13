47f7b6c457d1ced840cd2432017ffe57c1f7b13f
Mic-J-Lee <mleenavy@gmail.com>
2019-09-13 10:28:54 -0500

    Update bowling_spec.py

--- bowling_spec.py
+++ bowling_spec.py
@@ -2,7 +2,7 @@ import unittest
 from bowling import Game
 
 class TestGame(unittest.TestCase):
-  def setUp(self):
-    self.game = Game()
+    def setUp(self):
+        self.game = Game()
   
-  # Your tests here. 
+    # Your tests here. 
