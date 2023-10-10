113576fb9371a78f6a76cdd048c805b01f1da96a
Derek Marion <derek.j.marion@gmail.com>
2023-10-10 15:36:16 -0700

    Create first version of Frame class

--- bowling.py
+++ bowling.py
@@ -1,3 +1,24 @@
+class Frame:
+    def __init__(self, frame_score=0, strike=False):
+        self._frame_score = frame_score
+        self._strike = strike
+
+    @property
+    def get_frame_score(self):
+        return self._frame_score
+    
+    @get_frame_score.setter
+    def set_frame_score(self, score):
+        self._frame_score = score
+    
+    @property
+    def get_strike(self):
+        return self._strike
+    
+    @get_strike.setter
+    def set_strike(self, strike: bool):
+        self._strike = strike
+
 class Game:
 
 
